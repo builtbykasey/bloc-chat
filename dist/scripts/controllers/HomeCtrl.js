ebd6d0674c4f7050de6bceae17d2f1527f29170b
Kasey Markham <kaseycm@gmail.com>
2017-01-26 21:07:19 -0500

    User story 2 complete

---
+++ dist/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,17 @@
+(function() {
+    function HomeCtrl(Room) {
+    
+        this.chatRooms = Room.all;
+        
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
+    
+})();
+
+
+
+
+ 
